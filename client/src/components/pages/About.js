a0cd113183291b3b116510a9eec901d67355e743
andy <an4ger@gmail.com>
2019-11-16 16:47:36 +0200

    create react app setup

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => (
+    <div>
+        <h1>About This App</h1>
+        <p className="my-1">
+            This is a contact keeper app
+        </p>
+        <div className="bg-dark p">
+            <strong>
+                Version: 1.0.0
+            </strong>
+        </div>
+    </div>
+)
+
+export default About
