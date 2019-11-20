e989d8a8d09044bad002c16d5618b4a03a3cfb49
andy <an4ger@gmail.com>
2019-11-20 22:00:14 +0200

    contact list, contact form

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import Contacts from '../contacts/Contacts'
+import ContactForm from '../../context/contact/ContactForm'
 
 const Home = () => (
-    <div>
-        Home
+    <div className={"grid-2"}>
+        <div>
+            <ContactForm/>
+        </div>
+        <div>
+            <Contacts/>
+        </div>
     </div>
 )
 
