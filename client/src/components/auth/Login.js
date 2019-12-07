34dadb27bf961065490794297128b42765aa3c7b
andy <an4ger@gmail.com>
2019-12-07 16:55:47 +0200

    auth, navbar

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,12 +1,36 @@
-import React, {useState} from 'react'
+import React, {useState, useContext, useEffect} from 'react'
+import AlertContext from '../../context/alert/alertContext'
+import AuthContext from '../../context/auth/authContext'
 
-const Login = () => {
+const Login = (props) => {
+    const alertContext = useContext(AlertContext)
+    const authContext = useContext(AuthContext)
+
+    const {setAlert} = alertContext
+    const {login, error, clearErrors, isAuthenticated} = authContext
+
+    useEffect(() => {
+        if (isAuthenticated) {
+            props.history.push('/')
+        }
+
+        if (error === 'Invalid Credentials') {
+            setAlert(error, 'danger')
+            clearErrors()
+        }
+        // es-lint-disable-next-line
+    }, [error, isAuthenticated, props.history])
+
     const [user, setUser] = useState({email: '', password: ''})
     const {email, password} = user
     const onChange = e => setUser({...user, [e.target.name]: e.target.value})
     const onSubmit = e => {
         e.preventDefault()
-        console.log('login submit', user)
+        if (email === '' || password === '') {
+            setAlert('Please fill in all fields', 'danger')
+        } else {
+            login({email, password})
+        }
     }
     return (
         <div className="form-container">
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,9 +1,25 @@
-import React, {useState, useContext} from 'react'
+import React, {useState, useContext, useEffect} from 'react'
 import AlertContext from '../../context/alert/alertContext'
+import AuthContext from '../../context/auth/authContext'
 
-const Register = () => {
+const Register = (props) => {
     const alertContext = useContext(AlertContext)
+    const authContext = useContext(AuthContext)
+
     const {setAlert} = alertContext
+    const {register, error, clearErrors, isAuthenticated} = authContext
+
+    useEffect(() => {
+        if (isAuthenticated) {
+            props.history.push('/')
+        }
+
+        if (error === 'User already exists') {
+            setAlert(error, 'danger')
+            clearErrors()
+        }
+        // es-lint-disable-next-line
+    }, [error, isAuthenticated, props.history])
 
     const [user, setUser] = useState({name: '', email: '', password: '', password2: ''})
     const {name, email, password, password2} = user
@@ -16,6 +32,7 @@ const Register = () => {
             setAlert('Passwords do not match')
         } else {
             console.log('register submit', user)
+            register({name, email, password})
         }
     }
     return (
